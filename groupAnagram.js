d03accc1690898abb3ff742e68d80f0323c4f547
Reghuram Karunamurthi <54064162+reghuramk@users.noreply.github.com>
2023-12-02 18:02:48 +0000

    First commit

---
+++ groupAnagram.js
@@ -0,0 +1,16 @@
+const anagrams = (strs) => {
+    const map = new Map()
+
+    for (let str of strs) {
+        const sorted = str.split('').sort().join('')
+        if (map.has(sorted)) {
+            map.set(sorted, [...map.get(sorted), str])
+        } else {
+            map.set(sorted, [str])
+        }
+    }
+
+    return Array.from(map.values())
+}
+
+module.exports = { anagrams }
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const groupAnagrams = require('./groupAnagram')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+app.get('/groupAnagram', (req, res) => {
+
+  const result = groupAnagrams.anagrams(["eat", "tea", "tan", "ate", "nat", "bat", "silent", "listen"])
+  res.send(result)
+
 })
